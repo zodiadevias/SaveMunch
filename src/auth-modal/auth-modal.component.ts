a58f55253b9358150c581ba73755a675964ee308
Lance-Ceo <abuyolancebenedict@gmail.com>
2025-05-12 04:42:13 +0800

    finish header/ add Authmodal component

---
+++ src/auth-modal/auth-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-modal',
+  imports: [],
+  templateUrl: './auth-modal.component.html',
+  styleUrl: './auth-modal.component.css'
+})
+export class AuthModalComponent {
+
+}
