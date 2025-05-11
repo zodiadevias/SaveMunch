549cfac48099b6da6a26004cc92657de563996c9
Lance-Ceo <abuyolancebenedict@gmail.com>
2025-05-12 06:22:30 +0800

    Modal for login

--- src/auth-modal/auth-modal.component.ts
+++ src/auth-modal/auth-modal.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-modal',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './auth-modal.component.html',
   styleUrl: './auth-modal.component.css'
 })
 export class AuthModalComponent {
+  @Input() isOpen: boolean = false;
 
+  closeModal(): void {
+    this.isOpen = false;
+  }
 }
--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthModalComponent } from "../auth-modal/auth-modal.component";
 
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [AuthModalComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isModalOpen: boolean = false;
 
+openModal(): void {
+  this.isModalOpen = true;
+}
   whatAmI = 'guest';
 
 }
