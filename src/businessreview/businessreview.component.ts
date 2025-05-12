02d497cff29268257cd0cb8cbb19fc822736639b
Lance-Ceo <abuyolancebenedict@gmail.com>
2025-05-12 13:08:02 +0800

    business review added

--- src/businessreview/businessreview.component.ts
+++ src/businessreview/businessreview.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-businessreview',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './businessreview.component.html',
   styleUrl: './businessreview.component.css'
 })
 export class BusinessreviewComponent {
+  name = 'Tinapayan';
 
 }
