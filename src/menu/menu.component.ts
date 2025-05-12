1adf706ff74c5b049e149202ad4fabbb978df700
Lord Byron Dizon <dizon.lordbyron@gmail.com>
2025-05-12 12:37:16 +0800

    menu

--- src/menu/menu.component.ts
+++ src/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
